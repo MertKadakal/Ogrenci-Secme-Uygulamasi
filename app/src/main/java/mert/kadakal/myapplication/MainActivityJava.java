2be211724794bcad6d56695f67901cc7ca3d22bd
MertKadakal <mert.kadakal1629@gmail.com>
2024-08-05 22:06:53 +0300

    Metin üzerinde işlemler denemeleri yapıldı

--- app/src/main/java/mert/kadakal/myapplication/MainActivityJava.java
+++ app/src/main/java/mert/kadakal/myapplication/MainActivityJava.java
@@ -2,6 +2,8 @@ package mert.kadakal.myapplication;
 
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -9,11 +11,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivityJava extends AppCompatActivity {
 
+    private TextView txtMessage;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView txtMessage = (TextView) findViewById(R.id.mertkadakal_yazisi);
+        txtMessage = (TextView) findViewById(R.id.mertkadakal_yazisi);
+    }
+
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN && event.getKeyCode() == KeyEvent.KEYCODE_Q) {
+            // "q" tuşuna basıldığında çalışacak kod
+            txtMessage.setVisibility(View.INVISIBLE);
+            return true;
+        }
+        return super.dispatchKeyEvent(event);
     }
 }
