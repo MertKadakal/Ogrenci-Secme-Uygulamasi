48616421b23776c9e2cd83f985007b012184121b
MertKadakal <mert.kadakal1629@gmail.com>
2024-08-05 22:42:21 +0300

    Denemeler

--- app/src/main/java/mert/kadakal/myapplication/MainActivityJava.java
+++ app/src/main/java/mert/kadakal/myapplication/MainActivityJava.java
@@ -4,6 +4,7 @@ package mert.kadakal.myapplication;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -12,6 +13,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivityJava extends AppCompatActivity {
 
     private TextView txtMessage;
+    private Button btnMessage;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -19,12 +21,14 @@ public class MainActivityJava extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         txtMessage = (TextView) findViewById(R.id.mertkadakal_yazisi);
+        btnMessage = (Button) findViewById(R.id.buton1);
     }
 
     public boolean dispatchKeyEvent(KeyEvent event) {
         if (event.getAction() == KeyEvent.ACTION_DOWN && event.getKeyCode() == KeyEvent.KEYCODE_Q) {
             // "q" tuşuna basıldığında çalışacak kod
             txtMessage.setVisibility(View.INVISIBLE);
+            btnMessage.setText("buton2");
             return true;
         }
         return super.dispatchKeyEvent(event);
